9ec2184e2eab5a40490609513e7cc4bf5654330d
Felipe augusto Belfort <fbelfort@42lyon.fr>
2024-03-26 16:39:06 +0100

    added comments and some clean up

--- Server.cpp
+++ Server.cpp
@@ -14,91 +14,151 @@ Server::~Server()
 
 }
 
-
-void	Server::launchServer(const std::string &port, const std::string &password)
+/**
+ * @brief 
+ * It initiates the server. If the PORT is not a valid port or if socket() fails it returns false,
+ *  if bind() or listen() fails it closes the fd and return false.
+ * In case of success it pushs the server socket to the list of sockets and returns true. 
+ * The password is setted by default
+ * 
+ * @param port expected a int between MIN_PORT and MAX_PORT
+ * @param password 
+ * @return false
+ * @return true 
+ */
+bool	Server::initServer(const std::string &port, const std::string &password)
 {
-	float		p = atof(port.c_str());
-	int			server_socket;
+	float		p;
+	pollfd		server_socket;
 	sockaddr_in	server_addr;
-	char		buff[BUFF_SIZE];
-	int			read_ret = 1;
-	int	client_socket;
 
-	if (port.empty() || port.find_first_not_of("0123456789") != std::string::npos || p < MIN_PORT || p > MAX_PORT)
-		return;
+
+	if (port.empty() || port.find_first_not_of("0123456789") != std::string::npos) 
+		return false;
+	p = atof(port.c_str());
+	if (p < MIN_PORT || p > MAX_PORT)
+		return false;
+
 	this->_port = p;
 	this->_password = password;
 
-	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_socket == -1)
-		return; // TODO add error message and error return
+	server_socket.fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_socket.fd == -1)
+		return false; 
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(this->_port);
 	server_addr.sin_addr.s_addr = INADDR_ANY;
 
-	if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)))
-		return;
-	if (listen(server_socket, MAX_CONNECTIONS))
-		return;
+	if (bind(server_socket.fd, (struct sockaddr *)&server_addr, sizeof(server_addr)))
+		return (close(server_socket.fd), false);
+	if (listen(server_socket.fd, MAX_CONNECTIONS))
+		return (close(server_socket.fd), false);
+	server_socket.events = POLLIN;
 
-	pollfd	socket;
-	socket.fd = server_socket;
-	socket.events = POLLIN;
+	this->_sockets.push_back(server_socket);
+	return true;
+}
 
-	std::vector<pollfd>	sockets;
+/**
+ * @brief 
+ * It calls the initServer() and if it returns true 
+ *  then it calls the signal() to catch 'ctrl+c' and launchs the loop with pool() to listen 
+ * the sockets
+ *  
+ * TODO => error management and the actions inside the for loop
+ * 
+ * @param port  to be used in initServer() 
+ * @param password  to be used in initServer()
+ */
+void	Server::launchServer(const std::string &port, const std::string &password)
+{
+	char		buff[BUFF_SIZE];
+	int			read_ret;
 
-	sockets.push_back(socket);
+	if (!this->initServer(port, password))
+		return; // TODO error msg
 
-	// std::string	response = "001: Welcome! \r\n";
 
-
+	std::string	test;
+	signal(SIGINT, &exitServer);
 	while (is_connected)
 	{
-		signal(SIGINT, &exitServer);
+		test.clear();
 
-		sockets[0].revents = 0;
-		int ret = poll(&sockets[0], sockets.size(), 10);
+		this->_sockets[0].revents = 0;
+		int ret = poll(&this->_sockets[0], this->_sockets.size(), 10);
 		if (ret < 0)
 			break; // error in poll()
 		else if (!ret)
 			continue; // timeout
-		if (sockets[0].revents & POLLIN)	// TODO => create a new client
-			client_socket = accept(server_socket, NULL, NULL);
-		for (size_t i = 1; i < sockets.size(); i++)
+		if (this->_sockets[0].revents & POLLIN)	// TODO => create a new client
+			this->createUser();
+			// client_socket = accept(this->_sockets[0].fd, NULL, NULL);
+		for (size_t i = 1; i < this->_sockets.size(); i++)
 		{
+			if (this->_sockets[i].revents & POLLIN)
+			{
+				// do {
+					memset(buff, 0, BUFF_SIZE);
+					read_ret = recv(this->_sockets[i].fd, buff, BUFF_SIZE, MSG_DONTWAIT);
+					buff[read_ret] = 0;
+					// std::cout << " client => " << this->_sockets[i].fd << " read_ret => " << read_ret << " -> " << buff;
+					test = " client => ";
+					test += (char)this->_sockets[i].fd + '0';
+					test += " read_ret => ";
+					test += (char)read_ret + '0';
+					test += " -> ";
+					test += buff;
+				// }	while (read_ret > 0 && is_connected);
 
-			do {
-				read_ret = recv(client_socket, buff, BUFF_SIZE, 0);
-				buff[read_ret] = 0;
-				std::cout << " sockets => " << sockets[i].fd << " client => " << client_socket << " -> " << buff;
-			}	while (read_ret > 0 && is_connected);
+			}
+			if (this->_sockets[i].revents & POLLOUT)
+				if (!test.empty())
+					send(this->_sockets[i].fd, test.c_str(), test.size(), MSG_DONTWAIT);
+
+			if (this->_sockets[i].revents & POLLERR)
+				std::cout << "client => " << this->_sockets[i].fd << " POLLERR" << std::endl;
+			if (this->_sockets[i].revents & POLLHUP)
+				std::cout << "client => " << this->_sockets[i].fd << " POLLHUP" << std::endl;
+				
+				// std::cout << " client => " << this->_sockets[i].fd << " ready to POLLOUT " ;
 
 
-			sockets[i].revents = 0;
+
+			this->_sockets[i].revents = 0;
 		}
-		// else
-		// {
-
-		// 	std::cout << "\nbla\n";
-		// 	// send(client_socket, response.c_str(), response.size(), 0);
-		// }
 	}
 
-	// struct sockaddr_storage	client_addr;
-	// socklen_t	addr_size = sizeof(client_addr);
 
 
 
-	std::cout << "\b\b\nExiting Server..." << std::endl;
+	std::cout << "\nExiting Server..." << std::endl;
 }
 
+/**
+ * @brief 
+ * Fonction to create a user socket and push it in the list
+ * TODO => error management and creation of a User object
+ * 
+ */
 void	Server::createUser(void)
 {
 	pollfd	user_socket;
-	user_socket.fd = accept(server_socket, NULL, NULL);
+
+	user_socket.fd = accept(this->_sockets[0].fd, NULL, NULL);
+	if (user_socket.fd == -1)
+		return; // TODO error msg
+	// fcntl(user_socket.fd, F_SETFL, O_NONBLOCK);
 	user_socket.events = POLLIN | POLLOUT;
+	this->_sockets.push_back(user_socket);
 }
 
+/**
+ * @brief 
+ * Handle the SIGINT and changes the is_connected state to exit the server
+ * 
+ * @param sign 
+ */
 void	exitServer(int sign)
 {
 	if (sign == SIGINT)
--- Server.hpp
+++ Server.hpp
@@ -3,13 +3,18 @@
 
 # include <iostream>
 # include <signal.h>
+# include <unistd.h>
+# include <fcntl.h>
 # include <string>
+# include <cstring>
 # include <sys/socket.h>
 # include <poll.h>
 # include <netinet/in.h>
 # include <algorithm>
 # include <vector>
-
+# include <map>
+// # include "User.hpp"
+// # include "Channel.hpp"
 
 # define MIN_PORT 1023
 # define MAX_PORT 65535
@@ -17,8 +22,11 @@
 class Server
 {
 private:
-	uint16_t	_port;
-	std::string	_password;
+	uint16_t				_port;
+	std::string				_password;
+	std::vector<pollfd>		_sockets;
+	// std::map<int, User*>	_clients;
+	// std::map<std::string, Channel*> _channels;
 
 public:
 	static bool	is_connected;
@@ -26,6 +34,7 @@ public:
 	~Server();
 
 	void	launchServer(const std::string &port, const std::string &password);
+	bool	initServer(const std::string &port, const std::string &password);
 	void	createUser(void);
 };
 
